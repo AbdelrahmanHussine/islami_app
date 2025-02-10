53918e1060ce683475cbd8b25d3ae210d665aa57
Abdelrahman hussien <145324289+AbdelrahmanHussine@users.noreply.github.com>
2025-02-10 09:18:44 +0200

    +initial commit +home screen +quran tab +hadeth tab

---
+++ lib/ui/home/Hadeth/Chapter_title_hadeth.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:test_islam_app/ui/HadethDetailes/Hadeth_details_screen.dart';
+import 'package:test_islam_app/ui/home/Hadeth/hadeth_data_class.dart';
+class ChapterTitleHadeth extends StatelessWidget {
+  Hadeth hadeth;
+   ChapterTitleHadeth(this.hadeth);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pushNamed(HadethDetailsScreen.routName,
+        arguments: hadeth
+        );
+      },
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(10),
+        child: Text(hadeth.title,style: TextStyle(fontSize: 24,color: Colors.black),),
+        
+      ),
+    );
+  }
+}
--- lib/ui/home/Hadeth/hadeth_tab.dart
+++ lib/ui/home/Hadeth/hadeth_tab.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:test_islam_app/ui/home/Hadeth/Chapter_title_hadeth.dart';
+import 'package:test_islam_app/ui/home/Hadeth/hadeth_data_class.dart';
 
-class HadethTab extends StatelessWidget {
-  const HadethTab({super.key});
+class HadethTab extends StatefulWidget {
+    HadethTab({super.key});
 
   @override
+  State<HadethTab> createState() => _HadethTabState();
+}
+
+class _HadethTabState extends State<HadethTab> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.red,
+    if(AllAhadeath.isEmpty)
+      loudHadeathFile();
+    return Column(
+      children: [
+        Expanded(flex: 1,
+            child: Image(image: AssetImage('assets/images/hadeth_logo.png'))),
+        Container(width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+          border: Border.symmetric( horizontal: BorderSide(width: 2,color: Theme.of(context).primaryColor)),
+
+
+        ),
+            child: Text('Hadeth',style: TextStyle(
+              fontSize: 24
+            ),)),
+Expanded(flex: 3,
+  child: AllAhadeath.isEmpty ?Center(child: CircularProgressIndicator())
+  :
+  ListView.separated(
+      itemBuilder: (context, index) {
+        return ChapterTitleHadeth(AllAhadeath[index]);
+      },
+      separatorBuilder: (context, index) {
+        return Container(
+          color: Theme.of(context).primaryColor,
+          width: double.infinity,
+          height: 2,
+          margin: EdgeInsets.symmetric(horizontal: 60),
+
+        );
+      },
+      itemCount: AllAhadeath.length),
+)
+      ],
     );
+
+  }
+
+  List<Hadeth> AllAhadeath = [];
+
+  void loudHadeathFile()async{
+String fileContent =await rootBundle.loadString('assets/files/ahadeth.txt');
+List<String> hadthList= fileContent.trim().split('#');
+for(int i = 0;i<hadthList.length;i++){
+  String singleHadeth= hadthList[i];
+  List<String> hadeathLine= singleHadeth.trim().split('\n');
+  String title= hadeathLine[0];
+  hadeathLine.remove(0);
+  String content = hadeathLine.join('\n');
+  Hadeth hadeth = Hadeth(title, content);
+  AllAhadeath.add(hadeth);
+
+}
+
+setState(() {
+
+});
   }
 }
